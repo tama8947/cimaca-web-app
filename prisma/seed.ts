69cd37ce733e77362ef7c6752d30ce50cac863c4
orurando <ortalla_9@hotmail.com>
2023-09-01 21:49:16 -0500

    feat: test deploy new user to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,12 +6,12 @@ const prisma = new PrismaClient();
 
 async function main() {
   await prisma.user.upsert({
-    where: { email: "admin@gmail.com" },
+    where: { email: "admin@admin.com" },
     update: {},
     create: {
-      name: "Administrador",
-      last_name: "",
-      email: "admin@gmail.com",
+      name: "Usuario",
+      last_name: "Administrador",
+      email: "admin@admin.com",
       password: "$2a$10$mARD/qq0PY4YJwTG85HyZORkVISq7CZwT2Y3F/I1vU89UGnspnW62",
       role: "admin",
       state: State.enabled,
