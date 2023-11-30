1b8a53bff3a9c6c99f12b131e2e3e595e9eec599
orurando <ortalla_9@hotmail.com>
2023-11-29 23:31:54 -0500

    feat: change class to functional

--- src/app/api/services/email/email-service.ts
+++ src/app/api/services/email/email-service.ts
@@ -8,37 +8,40 @@ type SendEmailOptions = Mail.Options;
 
 type ParametersEmailService = { awsCredentials?: boolean }
 
-export default class EmailService {
-  private readonly transporter: Transporter<SMTPTransport.SentMessageInfo>;
-  private readonly awsCredentials: boolean;
-  constructor (params?: ParametersEmailService) {
-    this.awsCredentials = params?.awsCredentials ?? true;
+const createTransporter = (params: ParametersEmailService, awsCredentials: boolean): Transporter<SMTPTransport.SentMessageInfo> => {
+  if (awsCredentials) {
+    const ses = new aws.SES({
+      credentialDefaultProvider: defaultProvider, region: process.env.AWS_REGION
+    });
 
-    if (this.awsCredentials) {
-      const ses = new aws.SES({
-        credentialDefaultProvider: defaultProvider, region: process.env.AWS_REGION
-      });
-
-      this.transporter = nodemailer.createTransport({
-        SES: { ses, aws }
-      });
-    } else {
-      this.transporter = nodemailer.createTransport({
-        host   : process.env.HOST_EMAIL,
-        port   : parseInt(process.env.PORT_EMAIL as string),
-        secure : process.env.SECURE_EMAIL === 'true',
-        auth   : {
-          user : process.env.USER_EMAIL,
-          pass : process.env.PASSWORD_EMAIL
-        }
-      });
-    }
-  }
-
-  async sendEmail (options: SendEmailOptions) {
-    return await this.transporter.sendMail({
-      ...options,
-      from: process.env.USER_EMAIL_FROM
+    return nodemailer.createTransport({
+      SES: { ses, aws }
+    });
+  } else {
+    return nodemailer.createTransport({
+      host   : process.env.HOST_EMAIL,
+      port   : parseInt(process.env.PORT_EMAIL as string),
+      secure : process.env.SECURE_EMAIL === 'true',
+      auth   : {
+        user : process.env.USER_EMAIL,
+        pass : process.env.PASSWORD_EMAIL
+      }
     });
   }
-}
+};
+
+const sendEmail = async (transporter: Transporter<SMTPTransport.SentMessageInfo>, options: SendEmailOptions) => {
+  return await transporter.sendMail({
+    ...options,
+    from: process.env.USER_EMAIL_FROM
+  });
+};
+
+export const createEmailService = (params?: ParametersEmailService) => {
+  const awsCredentials = params?.awsCredentials ?? true;
+  const transporter = createTransporter(params || {}, awsCredentials);
+
+  return {
+    sendEmail: async (options: SendEmailOptions) => await sendEmail(transporter, options)
+  };
+};
